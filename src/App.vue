868eb84ca80d4da98b3f88c17874a2c1fcb7dc93
Patryk Gerczuk <Gezikus.pattonciuallegro@o2.pl>
2021-09-15 11:19:25 +0200

    dd

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld
+      :client="this.VueShowClient.currentClient"
+      @returnClient="addUserApp"
+    />
+    <h1>{{ mojedane.imie }}</h1>
+    <h1>{{ mojedane.nazwisko }}</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    VueShowClient: {},
+    mojedane: { imie: "", nazwisko: "" },
+  }),
+  mounted() {
+    this.VueShowClient = this.coachViewContext.binding.value;
+  },
+  methods: {
+    addUserApp(Object) {
+      this.mojedane = Object;
+    },
+  },
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,45 +1,62 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <section class="moreInfoAboutClient">
+      <h2>Dane klienta</h2>
+      <div class="aboutClient">
+        <p>
+          Imię: <span>{{ client.name }}</span>
+        </p>
+        <p>
+          Nazwisko:
+          <span>{{ client.surname }}</span>
+        </p>
+        <p>
+          Nazwa firmy:
+          <span>{{ client.companyName }}</span>
+        </p>
+        <p>
+          NIP: <span>{{ client.nip }}</span>
+        </p>
+        <p>
+          Adres: <span>{{ client.address }}</span>
+        </p>
+        <p>
+          Pesel: <span>{{ client.pesel }}</span>
+        </p>
+        <p>
+          Telefon: <span>{{ client.phoneNumber }}</span>
+        </p>
+        <p>
+          E-mail: <span>{{ client.email }}</span>
+        </p>
+      </div>
+      <v-btn @click="returnClient(clientt)">
+        To ja Patryk
+      </v-btn>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  emits: ["returnClient"],
+  data: () => {
+    return {
+      clientt: { imie: "Patryk", nazwisko: "Gerczuk" },
+    };
+  },
   props: {
-    msg: String
-  }
-}
+    client: Object,
+  },
+  methods: {
+    returnClient(object) {
+      this.$emit("returnClient", object);
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
